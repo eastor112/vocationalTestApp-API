2803c80e360c53c5ec3f0fd9c6b7996f45358d85
Emerson Asto <emerar.mct@gmail.com>
2022-03-26 09:59:40 -0500

    deleted comments

--- routes.js
+++ routes.js
@@ -1,13 +1,7 @@
-/**
- * Main application routes
- */
 const users = require('./api/users');
-// const universities = require('./api/universities');
 
 function routes(app) {
-  // API Routes
   app.use('/api/users', users);
-  // app.use('/api/universities', universities);
 }
 
 module.exports = routes;
