9edbb94dca8b7880ec355caafc725fd2929de60e
Emerson Asto <emerar.mct@gmail.com>
2022-03-30 00:33:25 -0500

    catched error in .pre save"

--- api/users/users.model.js
+++ api/users/users.model.js
@@ -38,14 +38,19 @@ const UserSchema = new mongoose.Schema({
 
 UserSchema.pre('save', async function (next) {
   const user = this;
+  try {
+    if (!user.isModified('password')) {
+      return next();
+    }
 
-  if (!user.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
 
-  const salt = await bcrypt.genSalt(10);
-
-  user.password = await bcrypt.hash(user.password, salt);
-
-  return next();
+    user.password = hash;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
 UserSchema.methods.comparePassword = async function (candidatePassword) {
