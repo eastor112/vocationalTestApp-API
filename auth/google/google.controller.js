b0f5db7348a52a970159d4ab8b4b824406cd1bee
Emerson Asto <emerar.mct@gmail.com>
2022-03-31 22:56:25 -0500

    fixed bug with invalid google token

--- auth/google/google.controller.js
+++ auth/google/google.controller.js
@@ -5,35 +5,41 @@ const googleVerify = require('../../helpers/googleTokenValidator');
 const handlerGoogleLogin = async (req, res) => {
   const { idToken } = req.body;
 
-  const { names, fatherName, motherName, email, profile } = await googleVerify(idToken);
+  try {
+    const { names, fatherName, motherName, email, profile } = await googleVerify(idToken);
 
-  let user = await User.findOne({ email });
+    let user = await User.findOne({ email });
 
-  if (!user) {
-    user = new User({
-      names,
-      fatherName,
-      motherName,
-      email,
-      profile,
-      google: true,
-      password: '(:P)',
+    if (!user) {
+      user = new User({
+        names,
+        fatherName,
+        motherName,
+        email,
+        profile,
+        google: true,
+        password: '(:P)',
+      });
+      user = await user.save();
+    }
+
+    if (!user.state) {
+      return res.status(400).json({
+        msg: 'Account was blocked or deleted',
+      });
+    }
+
+    const token = await signToken(user.public);
+
+    return res.status(200).json({
+      token,
+      user,
     });
-    user = await user.save();
-  }
-
-  if (!user.state) {
+  } catch (error) {
     return res.status(400).json({
-      msg: 'Account was blocked or deleted',
+      msg: 'Invalid google token',
     });
   }
-
-  const token = await signToken(user.public);
-
-  return res.status(200).json({
-    token,
-    user,
-  });
 };
 
 module.exports = handlerGoogleLogin;
