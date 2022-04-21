7229e611dbf0302dbc0e95b298f470ee36928e9e
Emerson Asto <emerar.mct@gmail.com>
2022-04-20 23:04:06 -0500

    test result required true

--- api/testResults/testResults.model.js
+++ api/testResults/testResults.model.js
@@ -4,6 +4,7 @@ const TestResultsSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
+    required: true,
   },
   test: {
     type: mongoose.Schema.Types.ObjectId,
